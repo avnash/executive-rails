eea0e994295b46afe49313efafa9539cbdd81be2
chiranjeevi <chiru6753@gmail.com>
2014-09-12 04:14:18 +0530

    Merged all changes to master

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140910141657) do
+ActiveRecord::Schema.define(version: 20140910234011) do
+
+  create_table "ckeditor_assets", force: true do |t|
+    t.string   "data_file_name",               null: false
+    t.string   "data_content_type"
+    t.integer  "data_file_size"
+    t.integer  "assetable_id"
+    t.string   "assetable_type",    limit: 30
+    t.string   "type",              limit: 30
+    t.integer  "width"
+    t.integer  "height"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "ckeditor_assets", ["assetable_type", "assetable_id"], name: "idx_ckeditor_assetable", using: :btree
+  add_index "ckeditor_assets", ["assetable_type", "type", "assetable_id"], name: "idx_ckeditor_assetable_type", using: :btree
 
   create_table "comments", force: true do |t|
     t.integer  "user_id",    null: false
@@ -70,8 +86,6 @@ ActiveRecord::Schema.define(version: 20140910141657) do
     t.float    "avg_rate",            default: 0.0,   null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "ups"
-    t.integer  "downs"
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
@@ -97,10 +111,14 @@ ActiveRecord::Schema.define(version: 20140910141657) do
   add_index "rates", ["user_id"], name: "index_rates_on_user_id", using: :btree
 
   create_table "tags", force: true do |t|
-    t.string   "name",        limit: 45, null: false
+    t.string   "name",                limit: 45, null: false
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "tags", ["name"], name: "index_tags_on_name", unique: true, using: :btree
@@ -125,7 +143,6 @@ ActiveRecord::Schema.define(version: 20140910141657) do
     t.timestamp "updated_timestamp"
     t.text      "usertype"
     t.integer   "notifications_count",             default: 0, null: false
-    t.string    "typename",            limit: 40
   end
 
   add_index "users", ["username"], name: "UNIQUE", unique: true, using: :btree
