daa68f0ff51475e0b30296cbd08954b99d2fe9e0
sai <saikiranp321@gmail.com>
2014-09-18 19:46:28 +0530

    bsdfs

--- db/schema.rb
+++ db/schema.rb
@@ -86,6 +86,8 @@ ActiveRecord::Schema.define(version: 20140910234011) do
     t.float    "avg_rate",            default: 0.0,   null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "ups"
+    t.integer  "downs"
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
@@ -111,14 +113,10 @@ ActiveRecord::Schema.define(version: 20140910234011) do
   add_index "rates", ["user_id"], name: "index_rates_on_user_id", using: :btree
 
   create_table "tags", force: true do |t|
-    t.string   "name",                limit: 45, null: false
+    t.string   "name",        limit: 45, null: false
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "avatar_file_name"
-    t.string   "avatar_content_type"
-    t.integer  "avatar_file_size"
-    t.datetime "avatar_updated_at"
   end
 
   add_index "tags", ["name"], name: "index_tags_on_name", unique: true, using: :btree
@@ -143,6 +141,7 @@ ActiveRecord::Schema.define(version: 20140910234011) do
     t.timestamp "updated_timestamp"
     t.text      "usertype"
     t.integer   "notifications_count",             default: 0, null: false
+    t.string    "typename",            limit: 40
   end
 
   add_index "users", ["username"], name: "UNIQUE", unique: true, using: :btree
