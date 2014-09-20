296b3981b775d2241126413851261cff9ebcd1c6
sai <saikiranp321@gmail.com>
2014-09-20 11:05:35 +0530

    ckeditor removed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,6 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require jquery_ujs
-//= require ckeditor/init
 //= require turbolinks
 //= require bootstrap-tooltip
 //= require bootstrap-popover
